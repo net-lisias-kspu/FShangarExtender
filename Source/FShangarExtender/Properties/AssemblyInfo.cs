385af55e20e2ad92693c9b95b48a20ce04ab11a0
Lisias T <me@lisias.net>
2019-04-27 04:49:03 -0300

    Properly identifying this fork as "/L"

--- Source/FShangarExtender/Properties/AssemblyInfo.cs
+++ Source/FShangarExtender/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FShangarExtender")]
+[assembly: AssemblyTitle("FShangarExtender /L Unofficial")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FShangarExtender")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
