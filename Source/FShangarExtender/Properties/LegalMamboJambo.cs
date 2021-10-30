f0d7f7b60267c73d7113482b7d2bcb4e877d0e11
Lisias T <me@lisias.net>
2021-10-30 11:35:24 -0300

    Legal mambo jambo

--- Source/FShangarExtender/Properties/LegalMamboJambo.cs
+++ Source/FShangarExtender/Properties/LegalMamboJambo.cs
@@ -2,7 +2,7 @@
 {
 	public static class LegalMamboJambo
 	{
-		public const string Company = "L Aerospace/KSP Division";
+		public const string Company = "/L Aerospace/KSP Division";
 		public const string Copyright = "© 2018-21 LisiasT";
 		public const string Product = "FShangarExtender";
 		public const string Trademark = "FShangarExtender™ by SNJO, Extended™ by LinuxGuruGamer, /L Unleashed™ by LisiasT";
